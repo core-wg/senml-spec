520979059a6bea4a90a8a5e1bae7c9d908747e08
EKR <ekr@rtfm.com>
2012-03-26 10:50:06 +0200

    Fix bug

--- ietfvote/static/js/ba_rate.js
+++ ietfvote/static/js/ba_rate.js
@@ -25,8 +25,10 @@ var poll_appspot = function(ba, last_time) {
 	       success:function(s) {
 		   js = JSON.parse(s);
 		   
-		   last_time = js.data[js.data.length - 1].time + 1;
-		   ba.update_plot(js.data);
+                   if (js.data.length) {
+		       last_time = js.data[js.data.length - 1].time + 1;
+		       ba.update_plot(js.data);
+                   }
 		   setTimeout(function() {
 				  poll_appspot(ba, last_time);
 			      },
